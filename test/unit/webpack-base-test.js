79aafd10aaef232981e928de61d976e20475c5c4
peng <pcj1593@163.com>
2020-04-02 20:21:29 +0800

    test: webpack.base.js test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -6,8 +6,6 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
   // eslint-disable-next-line no-undef
   it('entry', () => {
-    assert.equal(
-      baseConfig.entry.index, '/Users/pengchangjun/Documents/mystudyprogram/webpack_build_2020/test/smoke/template/src/index/index.js',
-    );
+    assert.equal(baseConfig.entry.index.indexOf('webpack_build_2020/test/smoke/template/src/index/index.js') > -1, true);
   });
 });
