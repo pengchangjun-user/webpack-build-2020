4f3769a8b3f93f779996568cb9b52ec481e138b7
peng <pcj1593@163.com>
2020-04-02 20:25:22 +0800

    test: update webpack-base-test.js

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -6,6 +6,6 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
   // eslint-disable-next-line no-undef
   it('entry', () => {
-    assert.equal(baseConfig.entry.index.indexOf('webpack_build_2020/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js') > -1, true);
   });
 });
